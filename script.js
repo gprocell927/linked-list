7e253d475e1379838ddf017ea9b7294f1353cf31
Maia Stone <maiadov@gmail.com>
2016-08-30 15:34:15 -0600

    site name populated

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // when user inputs new bookmark, it will display in user-bookmarks
 
-var siteName = document.querySelector('.bookmark-site-name');
+// var siteName = document.querySelector('.bookmark-site-name');
+//
+// var siteURL = document.querySelector('.bookmark-site-url');
 
-var siteURL = document.querySelector('.bookmark-site-url');
+var siteName = $('.bookmark-site-name');
+var createButton = $('.submit');
+
+
+createButton.on('click', function(event){
+  event.preventDefault();
+$('.user-site').append(siteName);});
