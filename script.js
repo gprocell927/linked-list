ad29471451be5890d0fd42374a6806f171262e64
Gabi Procell <gprocell927@gmail.com>
2016-08-30 14:58:46 -0600

    tried to work on printing user input

---
+++ script.js
@@ -0,0 +1,5 @@
+// when user inputs new bookmark, it will display in user-bookmarks
+
+var siteName = document.querySelector('.bookmark-site-name');
+
+var siteURL = document.querySelector('.bookmark-site-url');
