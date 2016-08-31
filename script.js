c946607f81e9687db2519a412121e56a9c872569
Gabi Procell <gprocell927@gmail.com>
2016-08-31 17:01:15 -0600

    refactored

--- script.js
+++ script.js
@@ -1,42 +1,66 @@
-// when user inputs new bookmark, it will display in user-bookmarks
-
-// var siteName = document.querySelector('.bookmark-site-name');
-//
-// var siteURL = document.querySelector('.bookmark-site-url');
+var $siteName = $('.bookmark-site-name');
+var $siteURL = $('.bookmark-site-url');
+var $createButton = $('.create-button');
 
 
-$('.create-button').on('click', function(event){
-  event.preventDefault();
+function toggleCreate() {
+    if ($siteName.val() === '' && $siteURL.val() === '') {
+      $createButton.prop('disabled', true);
+    } else {
+      $createButton.prop('disabled', false);
+    }
+}
 
- var siteName = $('.bookmark-site-name').val();
- var siteURL = $('.bookmark-site-url').val();
+// toggleCreate(siteName,siteURL);
 
- validateUserInput(siteName,siteURL);
+// this changes class of prop of read button
 
- // $("#bookmarks tr:last").after(myRow);
- $('tbody').append(`<tr class="newRow">
-   <td class="site-name-cell">${siteName}</td>
-   <td class="site-url-cell"><a href="${siteURL}">${siteURL}</a></td>
-   <td><button class="markRead">Mark As Read</button></td>
-    <td><button class="removeLink">Remove Bookmark</button></td>
- </tr>`);
 
- $('.markRead').on('click', function() {
-   $(this).toggleClass('.read');
-   if ($(this).text() === "Mark As Read"){
-    $(this).text('Mark As Unread');
+function toggleRead(parentRow){
+  parentRow.toggleClass('.read');
+  changeReadText(parentRow);
+}
+
+function changeReadText(buttonText){
+  if (buttonText.text() === 'Mark As Read') {
+   buttonText.text('Mark As Unread');
   } else {
-    $(this).text("Mark As Read");
-  }
+   buttonText.text('Mark As Read');
+ }
+}
 
+function addNewBookmark(){
+  $('tbody').append(`<tr class="newRow">
+     <td class="site-name-cell">${$siteName.val()}</td>
+     <td class="site-url-cell"><a href="${$siteURL.val()}">${$siteURL.val()}</a></td>
+     <td><button class="markRead">Mark As Read</button></td>
+     <td><button class="removeLink">Remove Bookmark</button></td>
+   </tr>`);
+ }
+
+ $('tbody').find('.bookmark')
+
+ function validateUserInput (title, url) {
+
+   if ($.isEmptyObject(title) || $.isEmptyObject(url)) {
+   alert('ERROR: Please enter a valid site name and URL.');}
+ }
+
+ $('.site-input').on('keyup', function(){
+   toggleCreate();
  });
 
-  $('.removeLink').on('click', function(){
+ $createButton.on('click', function(event){
+  event.preventDefault();
+  validateUserInput($siteName.val(), $siteURL.val());
+  addNewBookmark();
+  // toggleRead();
+  });
+
+  $('tbody').on('click', '.removeLink', function(){
     $(this).parents('tr').first().remove();
   });
-});
 
- function validateUserInput (site,url) {
-   if (($.isEmptyObject(site)) ||($.isEmptyObject(url))) {
-   alert("ERROR: Please enter a valid site name and URL.");}
- }
+  $('tbody').on('click','.markRead', function() {
+    toggleRead($(this));
+  });
