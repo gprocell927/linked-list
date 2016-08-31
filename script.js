6c3a6ab390faa2d46d5e7ada33e0b4ef0f61b367
Gabi Procell <gprocell927@gmail.com>
2016-08-31 07:53:42 -0600

    error message alerts if fields are empty, trying to get new rows added with input

--- script.js
+++ script.js
@@ -7,12 +7,19 @@
 
 $('.create-button').on('click', function(event){
   event.preventDefault();
-  var siteName = $('.bookmark-site-name').val();
-  // console.log(siteName);});
+ var siteName = $('.bookmark-site-name').val();
+ var siteURL = $('.bookmark-site-url').val();
+
+ $('#bookmarks').append($('<tr>').append(siteName))
+              .append($('<td>').append(siteURL));
+
+
+
+    if (($.isEmptyObject(siteName)) ||($.isEmptyObject(siteURL))) {
+    alert("ERROR: Please enter a valid site name and URL.");
+  }
+//   would like to refactor this, but I couldn't get this to work if I split it into it's own function and made local vars global.
+
 $('#test1').text(siteName);
-
-  var siteURL = $('.bookmark-site-url').val();
-  // console.log(siteURL);});
-  $('#test').text(siteURL);
-
+$('#test').text(siteURL);
 });
